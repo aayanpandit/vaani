32b88ee2ed0a3d1555c1ea97dc6dabb48c582446
Aayan Pandit <YOUR_GITHUB_EMAIL>
2026-06-29 12:34:48 +0530

    feat: add database foundation and alembic setup

---
+++ backend/alembic/versions/35c248f6c99e_initial_setup.py
@@ -0,0 +1,28 @@
+"""initial_setup
+
+Revision ID: 35c248f6c99e
+Revises: 
+Create Date: 2026-06-29 12:34:17.778205
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '35c248f6c99e'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
